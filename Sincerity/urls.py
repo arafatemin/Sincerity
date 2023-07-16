653d5792f4432011d0b8d882361c95b4092a7c06
arafat <jasurboy52@gmail.com>
2023-07-16 13:07:26 +0300

    commit

--- Sincerity/urls.py
+++ Sincerity/urls.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-# from django.template.defaults import url
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -13,11 +12,11 @@ urlpatterns = [
     path('', include('user.urls')),
     path('', include('Photos.urls')),
     path('', include('Books.urls')),
-    # url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    # url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
-# handler404 = 'Projects.views.handler404'
+handler404 = 'Projects.views.handler404'
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
