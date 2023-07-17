152c1a701322d8e78664b2b3b3eaf2a73f390d27
arafat <jasurboy52@gmail.com>
2023-07-17 16:27:14 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Sincerity/settings.py

--- Sincerity/urls.py
+++ Sincerity/urls.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import handler404
-from django.urls import re_path as url
-from django.views.static import serve
+# from django.conf.urls import handler404
+# from django.urls import re_path as url
+# from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Article.urls')),
@@ -12,11 +12,11 @@ urlpatterns = [
     path('', include('user.urls')),
     path('', include('Photos.urls')),
     path('', include('Books.urls')),
-    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    # url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    # url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
-handler404 = 'Projects.views.handler404'
+# handler404 = 'Projects.views.handler404'
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
