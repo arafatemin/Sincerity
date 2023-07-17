1bbe774ee1b44c1f451a75bd2a15214a82686133
arafat <jasurboy52@gmail.com>
2023-07-17 18:25:59 +0300

    Merge remote-tracking branch 'origin/master'

--- Projects/admin.py
+++ Projects/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
+from django.views.decorators.csrf import csrf_exempt
+
 from .models import *
 
 
-admin.site.register(Project)
-admin.site.register(Manager)
-admin.site.register(Experience)
-admin.site.register(Education)
-admin.site.register(CategoryEducation)
-admin.site.register(Contact)
+@csrf_exempt
+class MyAdminView(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+
+admin.site.register(Project, MyAdminView)
+admin.site.register(Manager, MyAdminView)
+admin.site.register(Experience, MyAdminView)
+admin.site.register(Education, MyAdminView)
+admin.site.register(CategoryEducation, MyAdminView)
+admin.site.register(Contact, MyAdminView)
